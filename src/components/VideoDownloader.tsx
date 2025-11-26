75d303c2c8d93b56ea7169ff1b95d5a4f8e79376
vasuBalli <vasuballi815@gmail>
2025-11-26 16:12:01 +0530

    T

--- src/components/VideoDownloader.tsx
+++ src/components/VideoDownloader.tsx
@@ -3,7 +3,7 @@ import { Download, Loader2, AlertCircle, CheckCircle } from 'lucide-react';
 
 const API_BASE =
   (import.meta.env.VITE_API_BASE_URL as string | undefined)?.replace(/\/$/, "") ||
-  "http://http://13.126.201.28:80";
+  "http://13.126.201.28:80";
 
 type VideoItem = {
   index: number;
